98a2597c9897124183d2440ca25aea5c6d95d326
keoni <kimkeonhwi991231@gmail.com>
2022-11-10 15:49:14 +0900

    First commit

---
+++ app/src/main/java/com/example/final_java_project/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.final_java_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                Intent intent = new Intent(getApplicationContext(), menu_activity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.button2:
+                break;
+        }
+    }
+}
